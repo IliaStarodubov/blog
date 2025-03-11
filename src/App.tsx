be10c521b723d2c042dd2da2fe8f7b61d095bec9
IliaStarodubov <m.telo@bk.ru>
2025-03-11 19:44:14 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+// import './App.css';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+function App() {
+	return (
+		<Div>
+			<div>123</div>
+		</Div>
+	);
+}
+
+export default App;
